d520d36edfa3eeb3be8549888e9e769c2e939ee7
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-02-15 15:24:17 +0100

    affichage favoris ok

--- src/pages/private/FavoritesPage.jsx
+++ src/pages/private/FavoritesPage.jsx
@@ -1,55 +1,64 @@
-//  Pour cette page, nous allons simplement afficher les tweets des personnes que l'utilisateur a mis en favoris.
-// Il faut être connecté pour y avoir accés
-
-import { useEffect, useState, useRef, useContext } from "react";
+import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../../context/userContext";
-// import { AuthContext } from "../../../context/AuthProvider";
-// import { useAllTweetsStore } from "../../../stores/AllTweetsStore";
-// import Tweet from "../../components/utilitiesComponents/Tweet";
-import { NavLink } from "react-router-dom";
+import { FIREBASE_URL } from "../../firebase-config";
+import { toast } from "sonner";
+import LoadingComponent from "../commonsComponents/toolsComponents/LoadingComponent";
+import Twit from "../commonsComponents/Twit";
 
 export default function FavoritesPage() {
   const { currentUserDatas } = useContext(UserContext);
-  // const { tweets } = useAllTweetsStore();
+  const [filteredTwits, setFilteredTwits] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  // Filtrer les tweets des utilisateurs suivis par l'utilisateur connecté
-  // const filteredTweets = tweets.filter((tweet) =>
-  //   user.followed.includes(tweet.author)
-  // );
+  useEffect(() => {
+    async function fetchTwitsFiltered() {
+      try {
+        const response = await fetch(FIREBASE_URL + "posts.json");
 
-  // Inverser l'ordre des tweets
-  // const reversedTweets = filteredTweets.slice().reverse();
+        if (!response.ok) {
+          throw new Error("Une erreur est survenue !");
+        }
+
+        const data = await response.json();
+
+        const tweetsArray = Object.entries(data).map(([id, twit]) => ({
+          id,
+          ...twit,
+        }));
+        const filteredTwits = tweetsArray.filter((twit) =>
+          currentUserDatas.users_followed.includes(twit.id_author)
+        );
+        setFilteredTwits(filteredTwits);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+        toast.error(error.message);
+      }
+    }
+
+    fetchTwitsFiltered();
+  }, [currentUserDatas.users_followed]);
+
+  if (loading) {
+    return <LoadingComponent />;
+  }
+
+  if (error) {
+    return <div className="pt-24 text-center">Une erreur est survenue !</div>;
+  }
 
   return (
-    <div>
-      {currentUserDatas.login}
-      {/* {user.followed.length !== 0 ? (
-        reversedTweets.map((tweet, index) => (
-          <Tweet key={index} tweet={tweet} />
-          // <Tweet key={tweet.tweet_id} tweet={tweet} />
-        ))
-      ) : (
-        <>
-        <div className="p-4 font-semibold text-white text-md md:text-2xl">
-          <h2 className="my-8 text-2xl text-center text-red-300 underline underline-offset-8 md:text-4xl">
-          Vous n'avez pas de favoris !
-          </h2>
-          <img
-            src="https://images.unsplash.com/photo-1542984385-2184d2ba45eb?q=80&w=2674&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="dame avec une loupe"
-            className="mb-8 rounded-xl whiteShadow"
-          />
-          <p>
-            N'hesitez pas à vous abonner à des utilisateurs pour voir leurs tweets ici.
-          </p>
-          <NavLink
-            to="/"
-            className="block w-1/2 p-4 mx-auto my-8 text-center bg-blue-500 rounded-lg hover:bg-blue-700"
-          >
-            Retour à l'accueil
-          </NavLink>
-        </div></>
-      )} */}
-    </div>
+    <>
+      <p className="sticky top-0 py-4 text-xl text-center border-b sm:text-2xl sm:py-6 md:py-8 md:text-3xl border-neutral-500 bg-neutral-900">
+        <span className="font-bold">{currentUserDatas.login}</span>, les Twits de tes créateurs favoris !
+      </p>
+      <div className="flex flex-col-reverse flex-grow text-xl">
+        {filteredTwits.map((twit) => (
+          <Twit key={twit.id} twit={twit} />
+        ))}
+      </div>
+    </>
   );
 }
