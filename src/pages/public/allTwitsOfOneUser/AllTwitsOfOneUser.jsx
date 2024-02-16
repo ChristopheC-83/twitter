491f6618fe17c7ddc405c19b15146b4f21159117
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-02-16 22:24:58 +0100

    pb affichage post user descrit réglé.

--- src/pages/public/allTwitsOfOneUser/AllTwitsOfOneUser.jsx
+++ src/pages/public/allTwitsOfOneUser/AllTwitsOfOneUser.jsx
@@ -1,7 +1,5 @@
-//  Qd on clique sur le nom d'un user
-// on arrive sur sa page perso grace à son id en url
-// son profil puis ses twits
-
+import { useEffect, useState } from "react";
+import { FIREBASE_URL } from "../../../firebase-config";
 import HeaderUser from "./components/HeaderUser";
 import TwitsListOfUser from "./components/TwitsListOfUser";
 
@@ -9,11 +7,48 @@ import { useParams } from "react-router-dom";
 
 export default function AllTwitsOfOneUser() {
   const { user_id } = useParams();
-  // console.log("user_id", user_id);
+  const [userExists, setUserExists] = useState(false);
+  
+  async function checkIfUserExists(user_id) {
+    try {
+      const response = await fetch(FIREBASE_URL + `users/${user_id}.json`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch user data");
+      }
+
+      const userData = await response.json();
+
+      return userData !== null;
+    } catch (error) {
+      console.error("Error checking if user exists:", error);
+      throw new Error("Failed to check if user exists");
+    }
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const exists = await checkIfUserExists(user_id);
+      setUserExists(exists);
+    }
+
+    fetchData();
+  }, [user_id]);
 
   return (
     <div>
-      <HeaderUser user_id={user_id} />
+      {userExists === null ? (
+        <p>Chargement...</p>
+      ) : userExists ? (
+        <HeaderUser user_id={user_id} />
+      ) : (
+        <p className="p-4 text-xl text-center">Il n'est plus inscrit, mais nous avons toujours ses posts !</p>
+      )}
       <TwitsListOfUser user_id={user_id} />
     </div>
   );
