3e933a53c8bf9bd803544fc67f208d4251d0a412
ChristopheC83 <christophe.chiappetta@gmail.com>
2024-02-17 14:04:05 +0100

    nouvel db, gestion 0 twit dans home

--- src/pages/private/profilPage/Profil.jsx
+++ src/pages/private/profilPage/Profil.jsx
@@ -1,7 +1,7 @@
 //  profil d'un utilisateur
 //  avec la page timeline de l'utilisateur connecté
 
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import LoadingComponent from "../../commonsComponents/toolsComponents/LoadingComponent";
 import AvatarManager from "./components/AvatarManager";
 import InfosUSer from "./components/InfosUSer";
@@ -9,13 +9,15 @@ import BiographyManager from "./components/BiographyManager";
 import DeleteAccount from "./components/DeleteAccount";
 import AvatarNameUser from "./components/AvatarNameUser";
 import UsersFollowed from "./components/UsersFollowed";
+import { UserContext } from "../../../context/userContext";
 
 export default function Profil() {
+  const { loading } = useContext(UserContext);
 
-  // if (loading) {
-  //   return <LoadingComponent />;
-  // }
-
+  if (loading) {
+    return <LoadingComponent />;
+  }
+  
   return (
     <div className="flex flex-col w-full p-4 rounded shadow-md mb-36 sm:p-6 md:p-8">
       <div className="flex flex-col">
@@ -24,7 +26,7 @@ export default function Profil() {
         <AvatarManager />
         <InfosUSer />
         <BiographyManager />
-        <UsersFollowed/>
+        <UsersFollowed />
       </div>
     </div>
   );
